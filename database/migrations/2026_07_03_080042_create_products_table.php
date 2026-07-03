0ca739e97dbb36f1949afe81a2ef12db241eac8a
Moses Muchoki <mosesmmuchoki@gmail.com>
2026-07-03 11:38:03 +0300

    configure migrations

--- database/migrations/2026_07_03_080042_create_products_table.php
+++ database/migrations/2026_07_03_080042_create_products_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description');
+            $table->decimal('price',8,2);
+            $table->integer('stock')->defaultValue(0);
+            $table->string('category');
             $table->timestamps();
         });
     }
--- database/migrations/2026_07_03_080501_create_cart_items_table.php
+++ database/migrations/2026_07_03_080501_create_cart_items_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id');
+            $table->integer('quantity')->defaultValue(1);
+            $table->timestamp('added_at')->useCurrent();
             $table->timestamps();
         });
     }
